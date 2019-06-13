efbaea07388cc732655e3ceaeabff046d7e32c2b
Acshi Haggenmiller <acshikh@umich.edu>
2019-06-13 18:30:47 -0400

    cleanup

--- scripts/p4r-env-setup.sh
+++ scripts/p4r-env-setup.sh
@@ -2,7 +2,9 @@
 if ! grep gcc-7.1.0 ~/.profile > /dev/null; then
     echo 'export PATH=/usr/um/gcc-7.1.0/bin/:/usr/um/git-2.14.1/bin/:/usr/um/vscode-1.32.3/bin/:$PATH' >> ~/.profile
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    echo "export PATH=$DIR:\$PATH" >> ~/.profile
+    echo "export PATH=$DIR:\$PATH" >> ~/.bashrc
+    source ~/.profile
+    source ~/.bashrc
 else
     echo "no changes made"
 fi
