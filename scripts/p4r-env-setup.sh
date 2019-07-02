230e9bccb506979416d9e1d2d01e8cbaedaeb800
Acshi Haggenmiller <acshikh@umich.edu>
2019-07-02 12:25:20 -0400

    better env setup script; support zsh

--- scripts/p4r-env-setup.sh
+++ scripts/p4r-env-setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if ! grep gcc-7.1.0 ~/.profile > /dev/null; then
-    echo 'export PATH=/usr/um/gcc-7.1.0/bin/:/usr/um/git-2.14.1/bin/:/usr/um/vscode-1.32.3/bin/:$PATH' >> ~/.profile
+    profileAddition='export PATH=/usr/um/gcc-7.1.0/bin/:/usr/um/git-2.14.1/bin/:/usr/um/vscode-1.32.3/bin/:$PATH'
+    echo "$profileAddition" >> ~/.profile
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    echo "export PATH=$DIR:\$PATH" >> ~/.bashrc
-    source ~/.profile
-    source ~/.bashrc
+    rcAdd="export PATH=$DIR:\$PATH"
+    echo "$rcAdd" >> ~/.bashrc
+    if [ -f ~/.zshrc ]; then
+        echo "$rcAdd" >> ~/.zshrc
+    fi
+    $profileAddition
+    $rcAdd
 else
     echo "no changes made"
 fi
