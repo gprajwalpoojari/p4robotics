3ddae855dca7c0c91692e7c1ab0ce3fdae7bb4fc
Acshi Haggenmiller <acshikh@umich.edu>
2019-08-08 11:55:32 -0400

    fixed env setup

--- scripts/p4r-env-setup.sh
+++ scripts/p4r-env-setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+if [[ $0 == $BASH_SOURCE ]]; then
+    echo "Please run: source $0"
+    exit 1
+fi
+
 if ! grep gcc-7.1.0 ~/.profile > /dev/null; then
     profileAddition='export PATH=/usr/um/gcc-7.1.0/bin/:/usr/um/git-2.14.1/bin/:/usr/um/vscode-1.32.3/bin/:$PATH'
     echo "$profileAddition" >> ~/.profile
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
     rcAdd="export PATH=$DIR:\$PATH"
     echo "$rcAdd" >> ~/.bashrc
     if [ -f ~/.zshrc ]; then
