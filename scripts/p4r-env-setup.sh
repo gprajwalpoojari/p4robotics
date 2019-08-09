61e688ea3e050cd8ee9d815c9d1ca78bb8f5bc3f
Acshi Haggenmiller <acshikh@umich.edu>
2019-08-09 11:41:22 -0400

    fixed env setup script

--- scripts/p4r-env-setup.sh
+++ scripts/p4r-env-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sourced_path=$_
-if [[ $0 != $sourced_path ]]; then
+startup_path=$_
+if [[ $startup_path == *"/bash" ]]; then
     echo "Please run: source $0"
     exit 1
 fi
@@ -13,7 +13,7 @@ if [ -f /usr/bin/modulecmd ] ; then
     module load vscode/1.32.3
 fi"
     echo "$profileAddition" >> ~/.profile
-    dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+    dir="$( cd "$( dirname "$startup_path" )" >/dev/null 2>&1 && pwd )"
     rcAdd="export PATH=$dir:\$PATH"
     echo "$rcAdd" >> ~/.bashrc
     if [ -f ~/.zshrc ]; then
@@ -22,6 +22,6 @@ fi"
     eval "$profileAddition"
     eval "$rcAdd"
 else
-    dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+    dir="$( cd "$( dirname "$startup_path" )" >/dev/null 2>&1 && pwd )"
     echo "no changes made"
 fi
