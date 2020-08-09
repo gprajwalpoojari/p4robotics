87595bb5ee28643a93d49ae14242406d050bc7e5
Acshi Haggenmiller <acshikh@umich.edu>
2020-08-08 23:51:56 -0400

    env setup updated because we aren't using CAEN computers this semester

--- scripts/p4r-env-setup.sh
+++ scripts/p4r-env-setup.sh
@@ -11,21 +11,23 @@ if ! echo $script_folder | grep "scripts" > /dev/null; then
     exit 1
 fi
 
-if ! grep "/usr/bin/modulecmd" ~/.profile > /dev/null || test $1; then
-    profileAddition="
-if [ -f /usr/bin/modulecmd ] ; then
-    module load gcc/9.1.0
-    module load git/2.14.1
-    module load vscode/1.32.3
-    module load cmake/3.12.0
-fi
-
-if [ -f \$HOME/.bashrc ]; then
-    source \$HOME/.bashrc
-fi
-"
+if ! grep "p4r-env-setup.sh" ~/.bashrc > /dev/null || test $1; then
+# Section only needed for the CAEN Lab computers
+#     profileAddition="
+# if [ -f /usr/bin/modulecmd ] ; then
+#     module load gcc/9.1.0
+#     module load git/2.14.1
+#     module load vscode/1.32.3
+#     module load cmake/3.12.0
+# fi
+#
+# if [ -f \$HOME/.bashrc ]; then
+#     source \$HOME/.bashrc
+# fi
+# "
 
     rcAddition="
+# Added by p4r-env-setup.sh
 export PS1='\[\e]0;\u: \w\a\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 export PATH=$script_folder:\$PATH
 "
